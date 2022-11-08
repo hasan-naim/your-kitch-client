bf5cd48c1c3c21c9356ddbe958f701e301f0e75c
hasan-naim <hasannaim6246@gmail.com>
2022-11-08 09:20:07 +0600

    added the navbar

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "../components/Navbar/Navbar";
+
+function Home() {
+  return (
+    <div>
+      <div>
+        <Navbar />
+      </div>
+    </div>
+  );
+}
+
+export default Home;

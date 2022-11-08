d08bbe030a6952dc23415f2deb9c2e01b42722b4
hasan-naim <hasannaim6246@gmail.com>
2022-11-08 16:42:17 +0600

    sign up page is designed

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -4,6 +4,8 @@ import { Link } from "react-router-dom";
 function Login() {
   const [inputText, setInputText] = useState({
     email: "",
+    name: "",
+    img: "",
     pass: "",
   });
   const [btnState, setBtnState] = useState(false);
@@ -18,9 +20,38 @@ function Login() {
       <div className="container">
         <div className="w-full max-w-lg bg-white rounded-lg mx-auto p-12">
           <h1 className="text-center text-neutral font-bold text-4xl lg:text-5xl mb-8 font-mono">
-            Login
+            Sign Up
           </h1>
           <form onSubmit={handleSubmit}>
+            <div className="flex flex-col gap-2 mt-4">
+              <label htmlFor="Email" className="font-medium">
+                Name
+              </label>
+              <input
+                value={inputText.name}
+                onChange={(e) =>
+                  setInputText({ ...inputText, name: e.target.value })
+                }
+                type="text"
+                required
+                placeholder="Your Name"
+                className="input input-bordered w-full bg-white"
+              />
+            </div>
+            <div className="flex flex-col gap-2 mt-4">
+              <label htmlFor="Email" className="font-medium">
+                Profile Picture
+              </label>
+              <input
+                value={inputText.img}
+                onChange={(e) =>
+                  setInputText({ ...inputText, img: e.target.value })
+                }
+                type="text"
+                placeholder="Only Url"
+                className="input input-bordered w-full bg-white"
+              />
+            </div>
             <div className="flex flex-col gap-2 mt-4">
               <label htmlFor="Email" className="font-medium">
                 Email
@@ -62,16 +93,10 @@ function Login() {
                 className="btn btn-primary btn-block"
                 type="submit"
               >
-                Login
+                Sign Up
               </button>
             </div>
           </form>
-          <button
-            disabled={btnState}
-            className="mt-8 btn-block btn btn-outline btn-success text-success"
-          >
-            GOOGLE
-          </button>
         </div>
       </div>
     </div>
